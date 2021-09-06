7d2a1c84a8901b759dbaa7bba7e2bf0a4e4f78b4
Epinder Singh <epindersingh@Epinders-MBP.lan>
2021-09-06 13:40:58 +0800

    Adding Song and player components and styling to them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './styles/app.scss';
+
+import Song from './components/Song';
+import Player from './components/Player';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
